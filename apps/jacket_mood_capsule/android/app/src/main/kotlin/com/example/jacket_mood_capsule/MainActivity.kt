beb6b38f3c51c47649ddbacd711e62821ff3c055
nick.liao <nick.liao@192.168.1.198>
2026-04-22 00:24:42 +0800

    feat: upd

--- apps/jacket_mood_capsule/android/app/src/main/kotlin/com/example/jacket_mood_capsule/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jacket_mood_capsule
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- apps/jacket_mood_capsule/lib/boot/remote_config_endpoint.dart
+++
@@ -1,4 +0,0 @@
-/// Remote config endpoint for this app (aka `remote_url`).
-///
-/// Each jacket app SHOULD use a different endpoint.
-const String remoteConfigEndpoint = 'https://69e1e92fb1cb62b9f31779a9.mockapi.io/api2';
